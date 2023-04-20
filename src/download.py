0b8df5fb741883f9a2e02a8824c6e1a42b74cc09
Vincent <b07209016@ntu.edu.tw>
2023-04-21 00:47:12 +0800

    Update downloader to download only current day file

--- src/download.py
+++ src/download.py
@@ -1,6 +1,7 @@
 from ftplib import FTP
 from config.path_config import RAW_PATH
 from config.ftp_config import FTP_IP, USER_NAME, USER_PASS
+from datetime import datetime as dtmdtm, timedelta as dt
 
 ftp = FTP(FTP_IP)
 ftp.login(USER_NAME, USER_PASS)
@@ -10,7 +11,12 @@ log_files = ftp.nlst()
 print(log_files)
 
 for file_name in log_files:
-    with open(RAW_PATH / file_name, "wb") as fo:
-        ftp.retrbinary(f"RETR {file_name}", fo.write)
+    utc = dtmdtm.utcnow()
+    target = [(utc - dt(minutes=10)).strftime("%m%d"), utc.strftime("%m%d")]
+    if target[0] in file_name or target[1] in file_name:
+        with open(RAW_PATH / file_name, "wb") as fo:
+            ftp.retrbinary(f"RETR {file_name}", fo.write)
+        with open(RAW_PATH / ".." / "ftp.txt", "w") as fo:
+            fo.write(dtmdtm.now().strftime("%Y/%m/%d %H:%M:%S"))
 
 ftp.quit()
