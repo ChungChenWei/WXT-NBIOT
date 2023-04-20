dc9bd738d1d3ba2cb35eb84bb5d2001fa039a012
Vincent <b07209016@ntu.edu.tw>
2023-04-21 00:48:43 +0800

    Update downloader with command to download all files

--- src/download.py
+++ src/download.py
@@ -1,7 +1,11 @@
+import sys
+
+from datetime import datetime as dtmdtm
+from datetime import timedelta as dt
 from ftplib import FTP
-from config.path_config import RAW_PATH
+
 from config.ftp_config import FTP_IP, USER_NAME, USER_PASS
-from datetime import datetime as dtmdtm, timedelta as dt
+from config.path_config import RAW_PATH
 
 ftp = FTP(FTP_IP)
 ftp.login(USER_NAME, USER_PASS)
@@ -13,7 +17,7 @@ print(log_files)
 for file_name in log_files:
     utc = dtmdtm.utcnow()
     target = [(utc - dt(minutes=10)).strftime("%m%d"), utc.strftime("%m%d")]
-    if target[0] in file_name or target[1] in file_name:
+    if target[0] in file_name or target[1] in file_name or len(sys.argv) > 1:
         with open(RAW_PATH / file_name, "wb") as fo:
             ftp.retrbinary(f"RETR {file_name}", fo.write)
         with open(RAW_PATH / ".." / "ftp.txt", "w") as fo:
