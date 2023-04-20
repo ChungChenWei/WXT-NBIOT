646384b3d9576c8b0b9b39c76981418589a99b63
Vincent <b07209016@ntu.edu.tw>
2023-04-21 01:33:06 +0800

    Update spliter with hash compare

--- src/spliter.py
+++ src/spliter.py
@@ -1,6 +1,10 @@
-import pandas as pd
+import json
 from datetime import datetime as dtmdtm
-from config.path_config import mkdir_if_not_exist, L0_PATH, RAW_PATH
+from hashlib import md5
+
+import pandas as pd
+
+from config.path_config import DATA_DIR, L0_PATH, RAW_PATH, mkdir_if_not_exist
 
 idx_start = 3  # position of data start in a line
 idx_end = -5
@@ -55,7 +59,7 @@ def check_line_valid(line: str):
 def dataset_processor(data):
     record = {}
     for dataset in [data[i : i + 4] for i in range(0, len(data), 4)]:
-        if "#" in dataset:
+        if "#" in dataset or len(dataset) != 4:
             continue
         val = dataset.pop(1)
         key = "".join(dataset)
@@ -70,9 +74,35 @@ def to_split_station_L0(df: pd.DataFrame):
         datetime = data.index[0]
         OUTPUT_PATH = L0_PATH / datetime.strftime("%Y%m%d")
         mkdir_if_not_exist(OUTPUT_PATH)
-        data.to_csv(OUTPUT_PATH / f"{name}.csv")
+        try:
+            data.to_csv(OUTPUT_PATH / f"{name}.csv")
+        except OSError as e:
+            print(e)
+
+
+def get_file_md5(filename):
+    hash_md5 = md5()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 
 if __name__ == "__main__":
-    for file in RAW_PATH.glob("20230414*.LOG"):
-        main(file)
+    try:
+        with open(DATA_DIR / "raw_hash.json", "r") as fi:
+            hash = json.load(fi)
+    except FileNotFoundError:
+        hash = {}
+
+    for file in RAW_PATH.glob("*.LOG"):
+        currnt_md5_hash = get_file_md5(file)
+        old_md5_hash = hash.get(file.name, "")
+        if old_md5_hash != currnt_md5_hash:
+            print(file.name)
+            main(file)
+            hash[file.name] = currnt_md5_hash
+            print(file.name, "updated")
+
+        with open(DATA_DIR / "raw_hash.json", "w") as fo:
+            json.dump(hash, fo, indent=2)
