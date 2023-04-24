680b4f8323101fbed39c7b625f66695bb1267078
b07209016 <b07209016@140.112.67.199>
2023-04-24 14:26:12 +0800

    Add crontab shell script

---
+++ src/job.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/download.py
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/spliter.py
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/plotter.py
