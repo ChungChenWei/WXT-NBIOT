1c84642456f69859cd4c4c74416a1f4d6089cdf6
Vincent <b07209016@ntu.edu.tw>
2023-05-03 01:59:47 +0800

    Add GrASS2

--- src/config/wxt_config.py
+++ src/config/wxt_config.py
@@ -2,6 +2,7 @@ WXT_META = {
     "ctop": {"TEMP": "CC1", "RH": "HP1", "ACCR": "VM1", "WS": "SM2", "WD": "AD2", "PRES": "PH1"},
     "btop": {"TEMP": "CC0", "RH": "HP0", "ACCR": "VM0", "WS": "SM1", "WD": "AD1", "PRES": "PH0"},
     "GrASS": {"TEMP": "CC0", "RH": "HP0", "ACCR": "VM0", "WS": "SM1", "WD": "AD1", "PRES": "PH0"},
+    "GrASS2": {"TEMP": "CC0", "RH": "HP0", "ACCR": "VM0", "WS": "SM1", "WD": "AD1", "PRES": "PH0"},
     "GrASS3": {"TEMP": "CC0", "RH": "HP0", "ACCR": "VM0", "WS": "SM1", "WD": "AD1", "PRES": "PH0"},
     "hwakang": {"TEMP": "CC0", "RH": "HP0", "ACCR": "VM0", "WS": "SM1", "WD": "AD1", "PRES": "PH0"},
 }
