6cd18618c911db332a617c005bb11f5d2111b5b5
Vincent <b07209016@ntu.edu.tw>
2023-05-03 01:50:30 +0800

    Update shell script

--- src/job.sh
+++ src/job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 /home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/download.py
-/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/spliter.py
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/raw_parser.py ftp
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/raw_parser.py sd
+/home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/L0_merger.py
 /home/b07209016/anaconda3/bin/python /home/b07209016/WXT-NBIOT/WXT-NBIOT/src/plotter.py
